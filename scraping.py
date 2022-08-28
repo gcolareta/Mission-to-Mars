02869769a3067c03c36fce6c74a4118cb4af1cc0
Giovanna Colareta <104762216+gcolareta@users.noreply.github.com>
2022-08-28 16:54:44 -0400

    Final

--- scraping.py
+++ scraping.py
@@ -13,15 +13,13 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-    hemi = hemi_image(browser)
 
     # Run all scraping functions and store results in a dictionary
-    data = {'Dict1':
-            {"news_title": news_title,
+    data = {"news_title": news_title,
              "news_paragraph": news_paragraph,
              "featured_image": featured_image(browser),
              "facts": mars_facts(),
-            "last_modified": dt.datetime.now()},
+            "last_modified": dt.datetime.now(),
             'hemi': hemi_image(browser)
          }
 
